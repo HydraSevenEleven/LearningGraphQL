a90be1c3689356a46412ad814b6ef446ff5e3eb9
daliacode <daliacode@gmail.com>
2023-06-11 17:55:29 +0200

    company-schema added

---
+++ sampleData.js
@@ -0,0 +1,31 @@
+const companies = [
+    {
+        id: '1',
+        name: 'Swift Delivery Services',
+        street: '1234 Speedy Street',
+        zip: "12345",
+        city: "Rapidville",
+        state: "TX",
+        country: "USA"
+    },
+    {
+        id: '2',
+        name: 'Tech Innovators Inc.',
+        street: '567 Innovation Avenue',
+        zip: "67890",
+        city: "Techville",
+        state: "CA",
+        country: "USA"
+    },
+    {
+        id: '3',
+        name: 'Golden Harvest Farms',
+        street: '789 Orchard Lane',
+        zip: "45678",
+        city: "Harvestville",
+        state: "NY",
+        country: "USA"
+    },    
+];
+
+module.exports = { companies }
---
+++ schema/schema.js
@@ -0,0 +1,41 @@
+const { companies } = require('../sampleData.js');
+
+const { 
+	GraphQLObjectType, 
+	GraphQLID, 
+	GraphQLString, 
+    GraphQLInt,
+	GraphQLSchema
+} = require('graphql');
+
+// Company Type
+const CompanyType = new GraphQLObjectType({   //by convention Uppercase
+	name: 'company',
+	fields: () => ({
+		id: {type: GraphQLID},
+		name: {type: GraphQLString},
+		street: {type: GraphQLString},
+        zip: {type: GraphQLInt},
+        city: {type: GraphQLString},
+        state: {type: GraphQLString},
+        country: {type: GraphQLString},
+	})
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		company: {
+			type: CompanyType,
+			args: { id: {type: GraphQLID} },
+			resolve(parent, args) { // here is where we want to return our data -> later here mongoose to get data from the database
+				return companies.find(company => company.id === args.id);
+		    }
+        }
+	}
+});
+
+// now we need to export the schema as a module
+module.exports = new GraphQLSchema({
+	query: RootQuery
+})
